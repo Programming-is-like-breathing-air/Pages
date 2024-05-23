adce3fcc0cef9eab6f930ea57a0143e8c6aa19c2
Hexin Zheng <hzhe5094@uni.sydney.edu.au>
2024-05-23 16:24:15 +1000

    Update Map
    npm install @radix-ui/react-tabs

--- src/app/business/page.tsx
+++ src/app/business/page.tsx
@@ -18,7 +18,7 @@ const Business = () => {
   const developmentRef = useRef<HTMLDivElement>(null);
   const memberRef = useRef<HTMLDivElement>(null);
   const projectRef = useRef<HTMLDivElement>(null);
-  const contactRef = useRef<HTMLDivElement>(null);
+  const mapRef = useRef<HTMLDivElement>(null);
 
   const scrollToSection = (ref: React.RefObject<HTMLDivElement>) => {
     ref.current?.scrollIntoView({ behavior: 'smooth', block: 'center' });
@@ -38,7 +38,7 @@ const Business = () => {
             <Button variant="link" className="text-black" onClick={() => scrollToSection(developmentRef)}>Development</Button>
             <Button variant="link" className="text-black" onClick={() => scrollToSection(memberRef)}>Team Member</Button>
             <Button variant="link" className="text-black" onClick={() => scrollToSection(projectRef)}>Project</Button>
-            <Button variant="link" className="text-black" onClick={() => scrollToSection(contactRef)}>Contact</Button>
+            <Button variant="link" className="text-black" onClick={() => scrollToSection(mapRef)}>Map</Button>
           </div>
         </div>
       </div>
@@ -49,24 +49,25 @@ const Business = () => {
           <PageHeader>
             <Announcement />
             <PageHeaderHeading className="hidden md:block">
-              Check out some examples
+            The University of Sydney Blocks Design System
             </PageHeaderHeading>
             <PageHeaderHeading className="md:hidden">Examples</PageHeaderHeading>
             <PageHeaderDescription>
-              Dashboard, cards, authentication. Some examples built using the
-              components. Use this as a guide to build your own.
+            Leadership Starts Here: Unleash Your Creative Potential with The
+            University of Sydney's Blocks Design System - Where Innovation Meets
+            Imagination, One Block at a Time.
             </PageHeaderDescription>
             <PageActions>
-              <Button>Hi</Button>
+              <Button variant={'default'}>Start Here</Button>
             </PageActions>
           </PageHeader>
         </div>
         <div>
           <img
-            src="/portfolio.jpg"
+            src="/spiral.jpg"
             alt="Portfolio Image"
             className="object-cover rounded-md"
-            style={{ width: '1500px', height: '800px' }}
+            style={{ width: '1500px', height: '900px' }}
           />
         </div>
       </div>
@@ -84,16 +85,16 @@ const Business = () => {
               <div>
                 <Card className="max-w-md bg-white rounded-md shadow-sm overflow-hidden">
                   <img
-                    src="/portfolio.jpg"
+                    src="https://i.nextmedia.com.au/News/Uni_Sydney.png"
                     alt="University Building"
                     className="w-80 h-60 object-cover object-center"
                   />
                   <CardHeader className="bg-gray-100 p-3">
-                    <CardTitle className="text-lg font-semibold">Contact us</CardTitle>
+                    <CardTitle className="text-lg font-semibold">Biodiversity Frontier</CardTitle>
                   </CardHeader>
                   <CardContent className="p-3">
                     <CardDescription className="text-sm mb-3">
-                      Submit a partnership enquiry, you can<br /> also make a donation by contacting<br /> the University revenue team.
+                    Learn how the University of Sydney's <br />Biology Faculty is making groundbreaking<br /> strides in the field of biology and <br />environmental sciences.
                     </CardDescription>
                     <Button className=" w-full py-4 px-4">
                       Explore more →
@@ -104,16 +105,16 @@ const Business = () => {
               <div>
                 <Card className="max-w-md bg-white rounded-md shadow-sm overflow-hidden">
                   <img
-                    src="/portfolio.jpg"
+                    src="/aboriginal_art.jpeg"
                     alt="University Building"
                     className="w-80 h-60 object-cover object-center"
                   />
                   <CardHeader className="bg-gray-100 p-3">
-                    <CardTitle className="text-lg font-semibold">Contact us</CardTitle>
+                    <CardTitle className="text-lg font-semibold">Preserving Heritage</CardTitle>
                   </CardHeader>
                   <CardContent className="p-3">
                     <CardDescription className="text-sm mb-3">
-                      Submit a partnership enquiry, you can<br /> also make a donation by contacting<br /> the University revenue team.
+                    Uncover the vibrant and diverse <br />culture of Australia's First Nations.<br />This article delves into the profound history<br /> expressions of Australian Aboriginal culture.
                     </CardDescription>
                     <Button className=" w-full py-4 px-4">
                       Explore more →
@@ -124,16 +125,16 @@ const Business = () => {
               <div>
                 <Card className="max-w-md bg-white rounded-md shadow-sm overflow-hidden">
                   <img
-                    src="/portfolio.jpg"
+                    src="/sydney_opera_house.jpg"
                     alt="University Building"
                     className="w-80 h-60 object-cover object-center"
                   />
                   <CardHeader className="bg-gray-100 p-3">
-                    <CardTitle className="text-lg font-semibold">Contact us</CardTitle>
+                    <CardTitle className="text-lg font-semibold">Design Excellence Shines</CardTitle>
                   </CardHeader>
                   <CardContent className="p-3">
                     <CardDescription className="text-sm mb-3">
-                      Submit a partnership enquiry, you can<br /> also make a donation by contacting<br /> the University revenue team.
+                    Experience the pinnacle of Australian <br /> design innovation as we delve into <br /> the winners and standout designs <br /> from the Australian Design Awards.
                     </CardDescription>
                     <Button className=" w-full py-4 px-4">
                       Explore more →
@@ -149,7 +150,7 @@ const Business = () => {
 
         <div ref={memberRef} className="mt-4 flex flex-col items-center mb-2" style={{ marginBottom: '50px' }}>
           <Label className="font-bold text-5xl mb-2">Team member</Label>
-          <Label className="font-bold text-xl mb-2">Updates from the development group and faculties</Label>
+          <Label className="font-bold text-xl mb-2">Here's an introduction to our illustrious members</Label>
 
         </div>
         <div className="flex flex-col items-center mb-2" style={{ marginBottom: '100px' }}>
@@ -158,7 +159,7 @@ const Business = () => {
         {/* Project part */}
           <div ref={projectRef} className="mt-4 flex flex-col items-center mb-2" style={{ marginBottom: '50px' }}>
             <Label className="font-bold text-5xl mb-2">Project</Label>
-            <Label className="font-bold text-xl mb-2">Updates from the development group and faculties</Label>
+            <Label className="font-bold text-xl mb-2">Here is a brief description of our excellent program</Label>
 
           </div>
           <div className="flex flex-col items-center mb-2" style={{ marginLeft: '10%',  marginRight: '10%',marginBottom: '100px' }}>
@@ -167,12 +168,14 @@ const Business = () => {
        
           </div>
         
-        
+          <div ref={mapRef} className="mt-4 flex flex-col items-center mb-2" style={{ marginBottom: '50px' }}>
+            <Label className="font-bold text-5xl mb-2">Map</Label>
+            <Label className="font-bold text-xl mb-2">You can find us here</Label>
 
-        {/* Contact part */}
-        {/* <div ref={contactRef} className="mt-4 flex items-center mb-2" style={{ marginTop: '500px' }}>
-          <div>Contact Info</div>
-        </div> */}
+          </div>
+          <div className="flex flex-col items-center mb-2" style={{ marginLeft: '10%',  marginRight: '10%',marginBottom: '100px' }}>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3938.7210545858634!2d151.18624108204475!3d-33.88820087012621!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12b1d4ab9ef1d9%3A0x1d017d69037a07c0!2sThe%20University%20of%20Sydney!5e0!3m2!1sen!2sau!4v1716444001018!5m2!1sen!2sau" width="1600" height="900" loading="lazy" ></iframe>
+       </div> 
       </div>
     </div>
   );
